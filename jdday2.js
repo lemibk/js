faee9783035385e4f9322e511c60b94ddb30069e
lemibk <lemibidikasenbeta@gmail.com>
2024-05-29 14:43:43 +0300

    main

--- jdday2.js
+++ jdday2.js
@@ -20,6 +20,7 @@ const person = {
         return this.lastName;
     }
 };
+
 person.firstName = "john";
 const stud = {};
 stud.grade = 23;
@@ -27,3 +28,14 @@ stud.sec = "A";
 let name1 = person['firstName'];
 console.log("Frist Name:" + name1)
 console.log(person.function1)
+let fullname = '';
+
+for (let data in person) {
+    fullname += person[data] + " ";
+
+}
+const arr = Object.values(person);
+let json = JSON.stringify(person)
+//document.getElementById('id1').innerHTML = json + ""
+let Str="Cs stud";
+console.log(str.tolowerCase);
